954c66954480e0344b362b033ffbf91ada624ce6
Conor Luddy <conorluddy@gmail.com>
2022-07-20 14:22:30 +0100

    WIP

--- creator/src/main.rs
+++ creator/src/main.rs
@@ -1,9 +1,10 @@
-use std::io::prelude::*;
 use std::fs;
-use std::env;
-use pulldown_cmark::{Parser, Options, html};
 use std::fs::File;
+use std::env;
 use std::path::PathBuf;
+use std::ffi::OsString;
+use std::io::prelude::*;
+use pulldown_cmark::{Parser, Options, html};
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -11,18 +12,35 @@ fn main() -> std::io::Result<()> {
     let output_path = &args[2];
 
     for entry in fs::read_dir(content_path).unwrap() {
+        let options = Options::empty();
         let entry = entry.unwrap();
         let path = entry.path();
-        if path.is_file() {
+        let os_filename = entry.path().file_stem().unwrap().to_owned();
+        let os_string = OsString::from(os_filename);
+        let filename = os_string.to_str().unwrap();
+
+
+        if path.is_file() { 
+            // TODO: if filename ends with .md
+
             let content = fs::read_to_string(path).unwrap();
-            let options = Options::empty();
             let parser = Parser::new_ext(&content, options);
-            let output_file: PathBuf = [output_path, "fixmyname.html"].iter().collect();
-            let mut html_output = String::new();
+
+            // TODO: Match name of input MD file
+            let output_file: PathBuf = [output_path, filename].iter().collect();
+
+            println!("Writing {:?}", output_file);
+
             let mut file = File::create(output_file)?;
+            
+            // Make HTML from MD
+            let mut html_output = String::new();
             html::push_html(&mut html_output, parser);
+
+            //
             file.write_all(html_output.as_bytes())?;
         } else {
+            // Recursion 
             println!("{:?} is a dir", path);
         }
     }
@@ -32,3 +50,8 @@ fn main() -> std::io::Result<()> {
 // ~/Development/w/web22/creator
 // $ cargo build
 // $ cargo run "../content/pages" "../dist/pages"
+
+// TODO:
+// - Filename
+// - Wrapping default HTML template with <head> etc
+// - Learn how to borrow properly
