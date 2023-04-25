74db64746508fbfdf7af361f57db2badd95113f1
Conor Luddy <conorluddy@gmail.com>
2023-04-25 16:33:13 +0100

    WIP...

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import BackgroundNoise from "./components/BackgroundNoise/BackgroundNoise";
 import GridBase from "./components/Grid/GridBase/GridBase";
 import InjectedContent from "./components/InjectedContent/InjectedContent";
+import HeroImage from "./components/HeroImage/HeroImage";
 
 GridBase();
 BackgroundNoise();
 InjectedContent();
+HeroImage();
